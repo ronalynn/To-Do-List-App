de2640e89e5ae04537cc1205c6e54cef7ef14448
Ronalyn Nanong <waternanay@gmail.com>
2023-04-10 19:37:30 +0100

    initialised some stuff and added some event listeners

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// initialising const variables using the given classes from the DOM API
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+// adding event listeners to the given elements
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
